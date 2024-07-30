1aebb66423e95e8e38247861362902972bdeda05
rashidsarkar <rashidrock558@gmail.com>
2024-07-30 06:06:38 +0600

    "Added react-reveal dependency and updated package-lock.json; modified Home.jsx to import and use SectionHeading components"

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,8 +1,11 @@
 import CollegeCards from "../../Components/CollegeCard/CollegeCards";
+import SectionHeading from "../../Components/TextEffect/SectionHeading/SectionHeading";
+import SectionHeading2 from "../../Components/TextEffect/SectionHeading/SectionHeading2";
 
 function Home() {
   return (
     <div>
+      <SectionHeading>Our Colleges</SectionHeading>
       <CollegeCards />
     </div>
   );
